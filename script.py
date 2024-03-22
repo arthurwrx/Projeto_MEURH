2577cfc4d5342ca3cf8c744a3ed8c8038d920a8e
arthurwrx <arthurwrx@outlook.com>
2024-03-21 23:12:36 -0300

    Está capturando o número da demanda corretamente

--- script.py
+++ script.py
@@ -2,9 +2,10 @@ import time
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
+from datetime import datetime
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+# options.add_argument('--headless')
 
 # Definir o caminho para o diretório de download
 caminho_download = {"download.default_directory": "C:\\Users\\ArthurLN\\Downloads"}
@@ -38,32 +39,72 @@ class My_RH:
 
         for i in range(len(num_demands)):
 
+
+            ### Esse Script Acessa as Demandas
             demand = f"//tr[@id='HelpDesk_listView_row_{str(i+1)}']"
+
+            teste_demands = driver.find_element(By.XPATH,demand).text
+            teste_demands = teste_demands.split(" ")
+            teste_demands = teste_demands[0]
+            
+            print(teste_demands)
             print(demand)
+            
             driver.find_element(By.XPATH,demand).click()
             time.sleep(4)
-            element_documents = driver.find_element(By.XPATH,"//a[@displaylabel='Documentos']").click()
+            element_document = driver.find_element(By.XPATH,"//a[@displaylabel='Documentos']").click()
             time.sleep(4)
             
+            ##Escolhi esse elemento para listar o número de arquivos na página
             elements_list_download = driver.find_elements(By.XPATH,"//a[@name='downloadfile']")
 
             print(len(elements_list_download))
             
-
+            
             ## Utilizei essa lógica para obter o link de download de cada documento disponível e em seguida
             ## Com o método get do chromedriver baixar pelo for
-             
             for i in range(len(elements_list_download)):
 
-                download_arquivo = elements_list_download[i]
+                ## Esse bloco faz uma visita a cada página de Documento
+                caminho_pag_docs = driver.find_elements(By.XPATH,f'//td[contains(@class, "relatedListEntryValues")]/span/a')
+                caminho_pag_doc_url = caminho_pag_docs[i]
+                caminho_pag_doc_url = caminho_pag_doc_url.get_attribute('href')
+
+                driver.get(caminho_pag_doc_url)
+
+                responsável = driver.find_element(By.XPATH,"//span[@data-field-type='owner']").text
+                print(responsável)
+
+                ## Porque existe dois elementos com o mesmo nome, 
+                ## e eu sei que o segundo sempre será o num downloads.
+                num_downloads = driver.find_element(By.XPATH,"(//span[@data-field-type='integer'])[2]").text
+                print(num_downloads)
+
+                download_arquivo = driver.find_element(By.XPATH,'//a[@title="Arquivo Download"]')
                 download_arquivo = download_arquivo.get_attribute('href')
-                
+                download_arquivo = driver.get(download_arquivo)
                 print(download_arquivo)
 
-                driver.get(download_arquivo)
+                data_criacao = driver.find_element(By.XPATH,"//span[@data-field-type='datetime']").text
+                print(data_criacao)
+
+                data_ultima_modificacao = driver.find_element(By.XPATH,"(//span[@data-field-type='datetime'])[2]").text
+                print(data_ultima_modificacao)
+
+                data_hora_atual = datetime.now()
+                data_hora_atual_str = data_hora_atual.strftime("%Y-%m-%d %H:%M:%S") 
+
+                driver.back()
+                time.sleep(1)
+               
+
+
 
             driver.get("https://test.flowpro.com.br/index.php?module=HelpDesk&view=List&app=SUPPORT")
             time.sleep(3)
+
+      
+
             
 
 
